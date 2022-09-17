2b42f287fa0d1ee3e57e78e77ff1f87fcf3c583a
brightbong92 <wkdgusqhd080@naver.com>
2022-09-17 20:53:49 +0900

    model / color divide

--- FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -3,6 +3,8 @@
 import SwiftUI
 
 struct ProductRow: View {
+    let product: Product
+    
     var body: some View {
         HStack {
             productImage
@@ -11,14 +13,14 @@ struct ProductRow: View {
         .frame(height: 150)
         .background(Color.primary.colorInvert())
         .cornerRadius(6)
-        .shadow(color: Color.primary.opacity(0.33), radius: 1, x:2, y:2)
+        .shadow(color: Color.primaryShadow, radius: 1, x:2, y:2)
         .padding(.vertical, 8)
     }
 }
 
 private extension ProductRow {
     var productImage: some View {
-        Image("apple")
+        Image(product.imageName)
             .resizable()
             .scaledToFill()
             .frame(width: 140)
@@ -28,12 +30,12 @@ private extension ProductRow {
     var productDescription: some View {
         VStack(alignment: .leading) {
             
-            Text("백설공주 사과")
+            Text(product.name)
                 .font(.headline)
                 .fontWeight(.medium)
                 .padding(.bottom, 6)
             
-            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+            Text(product.description)
                 .font(.footnote)
                 .foregroundColor(.secondary)
             Spacer()
@@ -54,11 +56,11 @@ private extension ProductRow {
             
             Image(systemName: "heart")
                 .imageScale(.large)
-                .foregroundColor(Color("peach"))
+                .foregroundColor(Color.peach)
                 .frame(width: 32, height: 32)
             
             Image(systemName: "cart")
-                .foregroundColor(Color("peach"))
+                .foregroundColor(Color.peach)
                 .frame(width: 32, height: 32)
         }
     }
@@ -68,6 +70,6 @@ private extension ProductRow {
 
 struct ProductRow_Previews: PreviewProvider {
     static var previews: some View {
-        ProductRow()
+        ProductRow(product: productSamples[0])
     }
 }
